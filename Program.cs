3bb7edc4a55d62a07cfae6f3de9ca010c3aef523
ArkDreal <andrey.o.leal@gmail.com>
2021-04-30 13:58:48 -0300

    Exercício da Idade concluído

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExercícioIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console . WriteLine ( " Em que ano você nasceu? " );
+            int  anoNasceu  =  int . Parse ( Console . ReadLine ());
+
+            Console . WriteLine ( " Qual é o ano atual? " );
+            int  anoAtual  =  int . Parse ( Console . ReadLine ());
+
+            int  idadePessoa  =  anoAtual  -  anoNasceu ;
+            int  idadeDia  =  idadePessoa  *  365 ;
+            float  idadeSemana  =  idadeDia  /  7 ;
+
+            Console . WriteLine ( " A sua idade é de: "  +  idadePessoa );
+            Console . WriteLine ( " A sua idade em semanas é: "  +  idadeSemana );
+        }
+    }
+}
